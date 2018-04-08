f0751ee238b4a75ba937c9f055543b7f78023fbb
anandad <anandad@mcmaster.ca>
2018-04-08 02:30:32 -0400

    Added expert, Updated UserInput model, connected EntryPage to blackboard, update views

--- app/src/main/java/com/mcmaster/blackBoard/identreefier/BlackBoard.java
+++ app/src/main/java/com/mcmaster/blackBoard/identreefier/BlackBoard.java
@@ -3,18 +3,85 @@ package com.mcmaster.blackBoard.identreefier;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.mcmaster.blackBoard.identreefier.Experts.Expert;
+import com.mcmaster.blackBoard.identreefier.Experts.LeafExpert;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BlackBoard extends AppCompatActivity {
+
+    private List<Expert> experts;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_black_board);
 
+        this.experts = new ArrayList<>();
+
         Intent intent = getIntent();
-        String message = intent.getStringExtra("userInput");
+        UserInput userInput = (UserInput) intent.getSerializableExtra("userInput");
+
         TextView textView = findViewById(R.id.textView5);
-        textView.setText(message);
+        textView.setText(userInput.treeType);
+        Log.v("blackboard-treetype",userInput.treeType);
+
+        //startIdentificationProcess();
     }
+
+    public void startIdentificationProcess(){
+        Expert leaf = new LeafExpert();
+
+
+        registerExpert(leaf);
+
+
+
+    }
+
+    public void registerExpert(Expert expert){
+        if (expert == null){
+            throw new NullPointerException("Null Expert");
+        }
+
+        if(!experts.contains(expert)){
+            experts.add(expert);
+        }
+    }
+
+    public void unregister(Expert expert){
+        experts.remove(expert);
+    }
+
+
+    public void expertEventTrigger(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void inspect(){
+
+
+    }
+
+    public void reset(){
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/mcmaster/blackBoard/identreefier/EntryPageScroll.java
@@ -0,0 +1,90 @@
+package com.mcmaster.blackBoard.identreefier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener ;
+
+public class EntryPageScroll extends Activity implements OnItemSelectedListener {
+
+    private UserInput userInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_page_scroll);
+
+        Spinner tree_type = (Spinner) findViewById(R.id.tree_type_spinner);
+        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.tree_type, android.R.layout.simple_spinner_item);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tree_type.setAdapter(aa);
+        tree_type.setOnItemSelectedListener(this);
+
+        Spinner leaf_arrangement = (Spinner) findViewById(R.id.leaflet_arrangement_spinner);
+        ArrayAdapter<CharSequence> aa2 = ArrayAdapter.createFromResource(this, R.array.l_arrangement, android.R.layout.simple_spinner_item);
+        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        leaf_arrangement.setAdapter(aa2);
+        leaf_arrangement.setOnItemSelectedListener(this);
+
+        Spinner leaf_type = (Spinner) findViewById(R.id.leaf_type_spinner);
+        ArrayAdapter<CharSequence> aa3 = ArrayAdapter.createFromResource(this, R.array.l_type, android.R.layout.simple_spinner_item);
+        aa3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        leaf_type.setAdapter(aa3);
+        leaf_type.setOnItemSelectedListener(this);
+
+        this.userInput = new UserInput();
+
+    }
+
+    /**
+     * String treeType;
+     * String leafGrouping;
+     * String leafEdge;
+     * String lobesOrNot;
+     * String leafBladeStructure;
+     * String leafBase;
+     * String leafShape;
+     * String leafBudArrangement
+     * String barkColour;
+     * String barkTexture;
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id){
+        switch (parent.getId()){
+            case R.id.tree_type_spinner:
+                String treeType = parent.getSelectedItem().toString().trim();
+                Log.v("input-tree-type: ", treeType);
+                userInput.treeType = treeType;
+                Log.v("input-tree-type: ",userInput.treeType);
+                break;
+            case R.id.leaflet_arrangement_spinner:
+                userInput.leafletArrangement = parent.getSelectedItem().toString().trim();
+                Log.v("input-leaflet-arng:",userInput.leafletArrangement);
+                break;
+            case R.id.leaf_type_spinner:
+                userInput.leafType = parent.getSelectedItem().toString().trim();
+                Log.v("input-leaf-type: ",userInput.leafType);
+                break;
+        }
+    }
+
+
+    //public void add
+    public void sendBlackBoardData(View view){
+        Intent intent = new Intent(this,BlackBoard.class);
+        intent.putExtra("userInput", userInput);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+}
