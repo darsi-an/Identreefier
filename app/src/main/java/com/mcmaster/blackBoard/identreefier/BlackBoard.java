223113e4fe0672b71eb2b4e10c5fda2720b054ff
anandad <anandad@mcmaster.ca>
2018-04-10 04:37:39 -0400

    Added Listview and displaying top 10 from tree results

--- app/src/main/java/com/mcmaster/blackBoard/identreefier/BlackBoard.java
+++ app/src/main/java/com/mcmaster/blackBoard/identreefier/BlackBoard.java
@@ -1,11 +1,11 @@
 package com.mcmaster.blackBoard.identreefier;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.mcmaster.blackBoard.identreefier.Experts.BarkExpert;
@@ -20,7 +20,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +34,9 @@ public class BlackBoard extends AppCompatActivity {
 
 
     public Map<String,String> list;
+
+    public TextView displayResults;
+    private ListView m_listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +53,11 @@ public class BlackBoard extends AppCompatActivity {
 
         Intent intent = getIntent();
         this.userInput = (UserInput) intent.getSerializableExtra("userInput");
-        TextView textView = findViewById(R.id.textView5);
+        //TextView textView = findViewById(R.id.textView5);
 
         this.list = userInput.getDetails();
         Log.v("list values: ",list.toString() );
-        textView.setText(list.toString());
+        //textView.setText(list.toString());
 
         Expert leaf = new LeafExpert(this);
         registerExpert(leaf);
@@ -70,10 +72,22 @@ public class BlackBoard extends AppCompatActivity {
 
         Collections.sort(tree_result);
         int i = 0;
+
+        String[] top10 = new String[10];
         for (Tree tree : tree_result) {
-            i++;
+            if(i >= 10){
+                break;
+            }
+            top10[i] = tree.getTreeName()+" "+tree.getLikelihood();
             Log.v(i+"", tree.getTreeName()+" "+tree.getLikelihood());
+            i++;
         }
+        m_listview = (ListView) findViewById(R.id.resultListView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, top10);
+        m_listview.setAdapter(adapter);
+
+
     }
 
 
